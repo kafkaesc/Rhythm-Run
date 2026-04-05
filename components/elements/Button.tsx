16534ad16e1df4ea710279c9b380154af587d284
kafkaesc <jared.a.hettinger@gmail.com>
2026-04-05 10:08:49 -0500

    Adding Tailwind colors. Adding button styles.

--- components/elements/Button.tsx
+++ components/elements/Button.tsx
@@ -1,22 +1,65 @@
 import { cn } from '@/lib/css-utils';
 
-type ButtonProps = React.ComponentPropsWithoutRef<'button'>;
+type ButtonProps = React.ComponentPropsWithoutRef<'button'> & {
+	buttonStyle?: 'black-white' | 'danger' | 'primary' | 'warning';
+};
 
 export default function Button({
-	// buttonStyle,
+	buttonStyle,
 	children,
 	className,
 	...props
 }: ButtonProps) {
-	return (
-		<button
-			className={cn(
-				'px-3 py-1 rounded-md bg-foreground text-background transition-colors cursor-pointer hover:opacity-90',
-				className,
-			)}
-			{...props}
-		>
-			{children}
-		</button>
-	);
+	if (!buttonStyle || buttonStyle === 'primary') {
+		return (
+			<button
+				className={cn(
+					'px-3 py-1 rounded-md bg-highlight text-dark transition-colors cursor-pointer hover:opacity-90',
+					className,
+				)}
+				{...props}
+			>
+				{children}
+			</button>
+		);
+	}
+	if (buttonStyle === 'black-white') {
+		return (
+			<button
+				className={cn(
+					'px-3 py-1 rounded-md bg-foreground text-background transition-colors cursor-pointer hover:opacity-90',
+					className,
+				)}
+				{...props}
+			>
+				{children}
+			</button>
+		);
+	}
+	if (buttonStyle === 'danger') {
+		return (
+			<button
+				className={cn(
+					'px-3 py-1 rounded-md bg-danger text-dark transition-colors cursor-pointer hover:opacity-90',
+					className,
+				)}
+				{...props}
+			>
+				{children}
+			</button>
+		);
+	}
+	if (buttonStyle === 'warning') {
+		return (
+			<button
+				className={cn(
+					'px-3 py-1 rounded-md bg-warning text-dark transition-colors cursor-pointer hover:opacity-90',
+					className,
+				)}
+				{...props}
+			>
+				{children}
+			</button>
+		);
+	}
 }
