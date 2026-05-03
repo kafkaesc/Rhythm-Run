1e778a1687df407d02c18e56ead16b49168ef75b
kafkaesc <jared.a.hettinger@gmail.com>
2026-05-03 11:18:34 -0500

    Updating Last.fm search track mocks

--- mocks/LfmSearchTrackMocks.ts
+++ mocks/LfmSearchTrackMocks.ts
@@ -17,3 +17,19 @@ export const LfmSearchFeelGoodInc: LfmSearchTrack = {
 	listeners: '3256135',
 	url: 'https://www.last.fm/music/Gorillaz/_/Feel+Good+Inc.',
 };
+
+export const LfmSearchPortionsForFoxes: LfmSearchTrack = {
+	name: 'Portions For Foxes',
+	artist: 'Rilo Kiley',
+	mbid: 'f9ba9861-194a-40e3-b1ed-6df9b811b48e',
+	listeners: '376349',
+	url: 'https://www.last.fm/music/Rilo+Kiley/_/Portions+For+Foxes',
+};
+
+export const LfmSearchSugarWereGoinDown: LfmSearchTrack = {
+	name: "Sugar, We're Goin Down",
+	artist: 'Fall Out Boy',
+	mbid: '',
+	listeners: '1516438',
+	url: "https://www.last.fm/music/Fall+Out+Boy/_/Sugar,+We%27re+Goin+Down",
+};
