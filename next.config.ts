5fdb1599c1614123b890e58086416139c0b46e6f
kafkaesc <jared.a.hettinger@gmail.com>
2026-05-07 17:18:21 -0500

    Passing env variables using next.config

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,12 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	env: {
+		GET_SONG_BPM_KEY: process.env.GET_SONG_BPM_KEY,
+		LAST_FM_KEY: process.env.LAST_FM_KEY,
+		SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+		SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+	},
+};
 
 export default nextConfig;
