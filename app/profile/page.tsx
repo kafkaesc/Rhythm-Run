a8483ae47674cff36d6b5e7b0207511b3d626bee
kafkaesc <jared.a.hettinger@gmail.com>
2026-05-25 00:35:30 -0500

    Adding the SuggestedArtistsCloud to the profile page

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,11 +3,12 @@
 import { signOut } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
 import { useEffect } from 'react';
-import SpotifyIcon from '@/components/icons/SpotifyIcon';
 import Button from '@/components/elements/Button';
 import H1 from '@/components/elements/H1';
 import H2 from '@/components/elements/H2';
 import P from '@/components/elements/P';
+import SpotifyIcon from '@/components/icons/SpotifyIcon';
+import SuggestedArtistsCloud from '@/components/SuggestedArtistsCloud';
 import { useSessionStatus } from '@/hooks/useSessionStatus';
 
 export default function Profile() {
@@ -22,15 +23,19 @@ export default function Profile() {
 
 	return (
 		<>
-			<H1>Profile</H1>
+			<H1 className="text-center">Profile</H1>
 			<H2>Connected Accounts</H2>
 			{session.spotifyAccessToken ? (
-				<div className="flex items-center gap-2">
-					<SpotifyIcon />
-					<P>Spotify — {session.user?.name}</P>
-					<Button buttonStyle="black-white" onClick={() => signOut()}>
-						Logout
-					</Button>
+				<div className="flex flex-col gap-4">
+					<div className="flex items-center gap-2">
+						<SpotifyIcon />
+						<P>Spotify — {session.user?.name}</P>
+						<Button buttonStyle="black-white" onClick={() => signOut()}>
+							Logout
+						</Button>
+					</div>
+					<P>Suggested Artists</P>
+					<SuggestedArtistsCloud />
 				</div>
 			) : (
 				<P>No accounts connected.</P>
