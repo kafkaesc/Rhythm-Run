9c18270485a281c0c2b4ee66b2c510f1e8791dea
kafkaesc <jared.a.hettinger@gmail.com>
2026-05-25 02:21:46 -0500

    Updating the Navbar to show the menu icon on mobile. Still WIP.

--- components/navigation/NavList.tsx
+++ components/navigation/NavList.tsx
@@ -3,7 +3,7 @@ import NavItem from '@/components/navigation/NavItem';
 /** The site navigation list */
 export default function NavList() {
 	return (
-		<ul className="flex gap-2">
+		<ul className="hidden sm:flex gap-2">
 			<NavItem href="/">Home</NavItem>
 			<NavItem href="/about">About</NavItem>
 		</ul>
--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
-import DarkModeToggle from '@/components/DarkModeToggle';
+import Drawer from '@/components/layout/Drawer';
 import NavList from '@/components/navigation/NavList';
+import DarkModeToggle from '@/components/DarkModeToggle';
 
 /** Top-level navigation bar with the site logo, nav links, and dark mode toggle */
 export default function Navbar() {
@@ -10,6 +11,7 @@ export default function Navbar() {
 				Rhythm Run
 			</Link>
 			<div className="flex items-center justify-between sm:justify-normal sm:gap-4">
+				<Drawer className="sm:hidden">Hello world</Drawer>
 				<NavList />
 				<DarkModeToggle />
 			</div>
