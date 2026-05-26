cdc33c6755cdffd6ccc8e65cf5dc0f5c5a4f1bd5
kafkaesc <jared.a.hettinger@gmail.com>
2026-05-25 21:29:51 -0500

    Adding stacked prop for NavList. Adding stacked NavList to the navigation drawer.

--- components/navigation/Navbar.tsx
+++ components/navigation/Navbar.tsx
@@ -9,14 +9,16 @@ export default function Navbar() {
 		<nav className="flex items-center py-2 sm:justify-between">
 			<div className="flex-1 flex items-center sm:hidden">
 				<Drawer title="Rhythm Run" headerRight={<DarkModeToggle />}>
-					Hello world, new navigation items coming soon
+					<div className="mx-1">
+						<NavList stacked />
+					</div>
 				</Drawer>
 			</div>
 			<Link className="text-3xl md:text-4xl font-bold" href="/">
 				Rhythm Run
 			</Link>
 			<div className="flex-1 flex items-center justify-end gap-4">
-				<NavList />
+				<NavList className="hidden sm:flex" />
 				<div className="hidden sm:block">
 					<DarkModeToggle />
 				</div>
