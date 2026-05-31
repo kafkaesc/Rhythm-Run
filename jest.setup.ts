f58e7a99d3142ad9a9270391af140dd85724a62c
kafkaesc <jared.a.hettinger@gmail.com>
2026-05-31 13:52:39 -0500

    Adding Scrollable component

--- jest.setup.ts
+++ jest.setup.ts
@@ -1,1 +1,9 @@
 import '@testing-library/jest-dom';
+
+// jsdom does not implement ResizeObserver, add it here so components that
+// call new ResizeObserver() don't throw errors in tests
+global.ResizeObserver = class ResizeObserver {
+	observe() {}
+	unobserve() {}
+	disconnect() {}
+};
